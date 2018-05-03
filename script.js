36e165340789028572927763ff83693ed271ff94
junw3i <cj@Juns-MacBook.local>
2018-05-03 15:52:54 +0800

    solution

--- script.js
+++ script.js
@@ -1,7 +1,40 @@
 var monkey = "hello";
+
+// console.log(tweets[0].user.name);
+
+class Tweet extends React.Component {
+  render() {
+    return (
+      <div class="tweet">
+        <p class="first-row">
+          <b class="float-left">{this.props.tweet.user.name}</b>
+          <div class="float-right">
+          @{this.props.tweet.user.screen_name}
+          </div>
+        </p>
+
+        <p>{this.props.tweet.text}</p>
+      </div>
+    )
+  }
+}
+
+class Twitter extends React.Component {
+  render() {
+    let allTweets = this.props.tweets.map( (tweet) => {
+                          return <Tweet tweet={tweet} />;
+                        });
+
+    return (
+      <div>
+        <h3>WELCOME TO TWITTER</h3>
+        {allTweets}
+      </div>
+    )
+  }
+};
+
 ReactDOM.render(
-    <div>
-      {monkey}
-    </div>,
+    <Twitter tweets={tweets}/>,
     document.getElementById('root')
 );
